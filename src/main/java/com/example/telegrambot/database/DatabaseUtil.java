8ea47f537048c526bb37f28a3492a18fbb4a06b8
Xusniddin Muxiddinov <97612267+mukhiddinov@users.noreply.github.com>
2024-07-14 15:20:53 +0500

    Update DatabaseUtil.java

--- src/main/java/com/example/telegrambot/database/DatabaseUtil.java
+++ src/main/java/com/example/telegrambot/database/DatabaseUtil.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DatabaseUtil {
 
     private static final String url = "jdbc:postgresql://localhost:5432/interview_bot";
-    private static final String user = "postgres";
-    private static final String password = "asal2009";
+    private static final String user = "YOUR_POSTGRESQL_USER";
+    private static final String password = "YOUR_PASSWORD";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, user, password);
